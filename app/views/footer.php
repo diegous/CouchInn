41b501958b15358436b03c7925c600e932dd2d15
Nahuel <Nahuel>
2016-05-30 21:23:59 -0300

    [Se agregaron referencias en el footer a otras paginas]

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,3 +1,5 @@
 <footer>
-	-- hola soy un footer --
+	<a href="couch_type_list.php">Tipos de couch -</a>
+	<a href="index.php">Pagina Principal -</a>
+	<a href="couch_list.php">Listado de couch -</a>
 </footer>
