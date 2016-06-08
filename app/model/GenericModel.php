45cfbe3af800b631174ba70403bdfea0921d93f3
Matias Joaquin Rodriguez <rodrimatt1985@gmail.com>
2016-06-08 20:24:24 -0300

    Agrego get_all_enabled() a GenericModel.

--- app/model/GenericModel.php
+++ app/model/GenericModel.php
@@ -17,6 +17,24 @@ abstract class GenericModel {
     return $result;
   }
 
+
+  public function get_all_enabled() {
+    $query = 'SELECT * FROM ' . static::$table_name . ' WHERE enabled=true';
+    $connection = get_connection();
+    $query_result = $connection->query($query);
+
+    $result = array();
+
+    while ($row = $query_result->fetch_assoc())
+      $result[] = static::new_object_from_array($row);
+
+    $query_result->close();
+    $connection->close();
+
+    return $result;
+  }
+
+
   public static function get_by_id($an_id) {
     $query = 'SELECT * FROM ' . static::$table_name . ' WHERE id=' . $an_id;
     $connection = get_connection();
