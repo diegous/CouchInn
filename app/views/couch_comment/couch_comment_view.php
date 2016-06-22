5ee451953fd44112a40923a0295516ee00e3d4bf
nahueldi <nahueldicriscio@hotmail.com>
2016-06-22 01:22:47 -0300

    cambios esteticos

--- app/views/couch_comment/couch_comment_view.php
+++ app/views/couch_comment/couch_comment_view.php
@@ -1,32 +1,35 @@
 <hr>
-  <? if(isset($_SESSION['user']) && ($_SESSION['user']->id <> $couch->user_id) && !($_SESSION['user']->is_admin)) : ?>
-  <form action="/couch_comment/couch_comment_create.php" method="post">
+<h2>Preguntas del Couch</h2>
+<? if(isset($_SESSION['user']) && ($_SESSION['user']->id <> $couch->user_id) && !($_SESSION['user']->is_admin)) : ?>
+<form action="/couch_comment/couch_comment_create.php" method="post">
+  <div class="form-group">
+    <input type="hidden" name="couch_id" value="<?= $couch->id ?>">
+    <input type="hidden" name="user_id" value="<?= $_SESSION['user']->id ?>">
     <div class="form-group">
-      <input type="hidden" name="couch_id" value="<?= $couch->id ?>">
-      <input type="hidden" name="user_id" value="<?= $_SESSION['user']->id ?>">
-      <div class="form-group">
-        <input id="question" class="form-control" type="text" name="question" required>
-      </div>
-      <button type="submit" class="btn btn-default">Preguntar</button>
+      <input id="question" class="form-control" type="text" name="question" required>
     </div>
-  </form>
+    <button type="submit" class="btn btn-default">Preguntar</button>
+  </div>
+  <hr>
+</form>
+<? else: ?>
+  <br>
 <? endif ?>
 
-<h2>Preguntas del Couch</h2>
 <? foreach ($comment_list as $couch_comment): ?>
-  <hr>
+  <br>
   <form  action="/couch_comment/couch_comment_update.php" method="post">
     <input type="hidden" name="id" value="<?= $couch_comment->id ?>">
     <p>
       <span class="glyphicon glyphicon-comment" style="color:blue" title="Pregunta" aria-hidden="true"></span>&nbsp;&nbsp;<?= $couch_comment->comment_question; ?>
     </p>
-
     <? if (!$couch_comment->comment_answer): ?>
       <? if(isset($_SESSION['user']) && ($_SESSION['user']->id == $couch->user_id)) : ?>
         <span class="glyphicon glyphicon-comment" style="color:lightblue" title="Pregunta" aria-hidden="true"></span>
         <input id="answer" class="form-control" type="text" name="respuesta" required>
         <br>
         <button type="submit" class="btn btn-default">Responder</button>
+        <br><br>
       <?endif?>
     <? else: ?>
       <p class="couch-answer">
